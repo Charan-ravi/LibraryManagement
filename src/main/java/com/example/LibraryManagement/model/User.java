e687baba245d661cce0da16777ecbf0a3c69a9c9
charan <charan2ravi@gmail.com>
2025-04-24 15:07:44 +0530

    MVC for User

---
+++ src/main/java/com/example/LibraryManagement/model/User.java
@@ -0,0 +1,27 @@
+package com.example.LibraryManagement.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "borrowedBy", cascade = CascadeType.ALL)
+    private List<Book> borrowedBooks = new ArrayList<>();
+
+}
---
+++ src/main/java/com/example/LibraryManagement/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.LibraryManagement.repository;
+
+import com.example.LibraryManagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, Long> {
+}
